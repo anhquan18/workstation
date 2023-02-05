696d7e471c36e7355421e967b278fc704350036e
anhquan18 <quan181219@gmail.com>
2023-02-05 16:45:08 +0900

    change way to install vim from build from source to apt package

--- setup/setup_vim.sh
+++ setup/setup_vim.sh
@@ -11,13 +11,8 @@ sudo rm -rf /usr/local/share/vim
 sudo rm /usr/local/bin/vim
 
 # install new vim and plugin dependency
-sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev
-cd /opt/ && sudo git clone https://github.com/vim/vim
-cd vim/
+sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev vim
 
-# build file
-sudo ./configure --with-features=huge --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --prefix=/usr/local --enable-pythoninterp=dynamic --enable-python3interp=dynamic --enable-rubyinterp=dynamic 
-sudo make && sudo make install
 vim --version
 
 # install dein and plugins
