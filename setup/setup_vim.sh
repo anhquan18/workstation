c1cd746c589ad9be6512db767f28421964863cd9
anhquan18 <quan181219@gmail.com>
2023-03-07 12:06:22 +0900

    update vim setup to latest version

--- setup/setup_vim.sh
+++ setup/setup_vim.sh
@@ -6,7 +6,7 @@ sudo apt install wget
 
 # remove current vim
 sudo apt-cache -i depends vim
-sudo apt remove --purge vim vim-runtime vim-common
+sudo apt remove --purge vim*
 sudo rm -rf /usr/local/share/vim
 sudo rm /usr/local/bin/vim
 
@@ -14,19 +14,17 @@ sudo rm /usr/local/bin/vim
 cd
 git clone https://github.com/vim/vim.git
 cd vim
-./configure --enable-pythoninterp --prefix=/usr
+./configure --with-features=huge --enable-python3interp
 sudo apt install ncurses-dev
-make -j6
-sudo make install
+make -j6 && sudo make install
 
 # install plugin dependency
 sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python3-dev ruby-dev
-
 
 # install dein and plugins
 cd
 wget https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh
 
 sh ./installer.sh ~/.vim/bundles
-cd ~/git/my_file/workstation/setup && cp ../dot_files/vimrc.txt ~/.vimrc
+cp ~/git/my_file/workstation/dot_files/vimrc.txt ~/.vimrc
 cd && rm installer.sh && vim .vimrc
